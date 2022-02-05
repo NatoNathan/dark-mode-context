236023251472cd4cfb8c9c2a0f7b46ccaa37f288
NatoNathan <nathan@nathantamez.dev>
2022-02-05 22:38:57 +0000

    demo deploy

---
+++ example/build/precache-manifest.231c4ede7a789f4469270d0328f6e76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d776f8a26188818c4d6c3cb00874b4c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ba077313460723f4281",
+    "url": "./static/css/main.1624d5f1.chunk.css"
+  },
+  {
+    "revision": "7da6eb8aa2084a4edb8e",
+    "url": "./static/js/2.2f389c7a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2f389c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba077313460723f4281",
+    "url": "./static/js/main.6f0471c7.chunk.js"
+  },
+  {
+    "revision": "4383833de613dd70235b",
+    "url": "./static/js/runtime-main.f1d5f370.js"
+  }
+]);
