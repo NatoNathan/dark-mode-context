12705a48d1a3cf93ebbcfea538519d5a43baf60e
Nathan Tamez <25773848+NatoNathan@users.noreply.github.com>
2022-02-05 17:25:23 +0000

    added example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-import { ExampleComponent } from 'dark-mode-context'
-import 'dark-mode-context/dist/index.css'
+import { useDarkModeContext, DarkModeProvider } from 'dark-mode-context'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+const MyButton = () => {
+  const { isDarkMode, toggleDarkMode} = useDarkModeContext()
+
+  return (
+    <button className='' onClick={toggleDarkMode}>
+      {isDarkMode ? 'Dark Mode' : 'Light Mode'}
+    </button>
+  )
 }
 
+const App = () => (
+  <DarkModeProvider injectDarkClass>
+    <div className='w-screen h-screen p-2 bg-slate-50 dark:bg-slate-700'>
+      <h1>Hello World</h1>
+      <MyButton />
+    </div>
+  </DarkModeProvider>
+)
 export default App
---
+++ example/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class', // or 'class'
+  theme: {},
+}
